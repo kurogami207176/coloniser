f4742d3fab2b6f9ddaea2f9d3a601f8186cbd36d
kurogami207176 <pitbugx@yahoo.com>
2020-10-17 16:25:42 +1100

    Init change

---
+++ android/src/com/alaindroid/coloniser/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.alaindroid.coloniser;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.alaindroid.coloniser.Coloniser;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Coloniser(), config);
+	}
+}
---
+++ desktop/src/com/alaindroid/coloniser/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alaindroid.coloniser.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alaindroid.coloniser.Coloniser;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Coloniser(), config);
+	}
+}
